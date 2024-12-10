beaf68bb335a831bd46d301c7130cb02b2ebcfb8
Yakutenko Dasha <yakutenkodasha@gmail.com>
2024-12-10 18:06:44 +0400

    код переделан под hibernate

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,28 +1,52 @@
 package jm.task.core.jdbc.util;
-import com.mysql.cj.jdbc.Driver;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+import java.util.Properties;
 
 public class Util {
+    private static SessionFactory sessionFactory;
 
-    private Util() {}
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = getConfiguration();
 
-    private static final String URL = "jdbc:mysql://localhost:3306/1.1.3";
-    private static final String LOGIN = "root";
-    private static final String PASSWORD = "Root5612.SOV9t";
+                configuration.addAnnotatedClass(User.class);
 
-    private static Connection connection;
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
 
-    public static Connection getConnection() {
-        try {
-            Driver driver = new Driver();
-            DriverManager.registerDriver(driver);
-
-            connection = DriverManager.getConnection(URL, LOGIN, PASSWORD);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
         }
-        return connection;
+        return sessionFactory;
+    }
+
+    private static Configuration getConfiguration() {
+        Configuration configuration = new Configuration();
+
+        Properties settings = new Properties();
+        settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+        settings.put(Environment.URL, "jdbc:mysql://localhost:3306/1.1.3?useSSL=false");
+        settings.put(Environment.USER, "root");
+        settings.put(Environment.PASS, "Root5612.SOV9t");
+        settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+
+        settings.put(Environment.SHOW_SQL, "true");
+
+        settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+
+        settings.put(Environment.HBM2DDL_AUTO, "create-drop");
+
+        configuration.setProperties(settings);
+        return configuration;
     }
 }
